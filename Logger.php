037201692acdffdc8e276a72cf6d3c47e7901ad1
SRujuta <100143130+SRujuta@users.noreply.github.com>
2022-06-07 18:43:00 +0530

    Add files via upload

--- Logger.php
+++ Logger.php
@@ -1,59 +1,37 @@
 <?php
-/**
- * @category Digitalriver
- * @package  Digitalriver_DrPay
- */
 namespace Digitalriver\DrPay\Logger;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Store\Model\ScopeInterface;
-use Monolog\Handler\HandlerInterface;
+use Psr\Log\LoggerInterface;
 
 /**
- * Class Digitalriver Logger
- *
- * @FIXME Refactor to no longer extend the base class, but instead use composition to inject an abstract logger (i.e.
- *        a LoggerInterface)
+ * Class SomeClass
+ * The class responsible for doing something
  */
 class Logger
 {
     /**
-     * @var ScopeConfigInterface
+     * @var LoggerInterface
      */
-    private $scopeConfig;
+    private $logger;
 
     /**
-     * Logger constructor.
-     * @param string             $name       The logging channel
-     * @param HandlerInterface[] $handlers   Optional stack of handlers, the first one in the array is called first,etc.
-     * @param callable[]         $processors Optional array of processors
-     * @param ScopeConfigInterface $scopeConfig
+     * SomeClass constructor.
+     * @param LoggerInterface $logger
      */
-    public function __construct(
-        \Psr\Log\loggerInterface $logger,
-        ScopeConfigInterface $scopeConfig
-    ) {
-        /**
-         * TODO: This should be eliminated with MAGETWO-53989
-         */
-        $this->logger = $logger;
-        $this->scopeConfig = $scopeConfig;
-    }
-    /**
-     * Adds a log record.
-     *
-     * @param  int     $level   The logging level
-     * @param  string  $message The log message
-     * @param  array   $context The log context
-     * @return bool    Whether the record has been processed
-     */
-    public function addRecord(int $level, string $message, array $context = []): bool
+    public function __construct(LoggerInterface $logger)
     {
-        $debug = $this->scopeConfig->getValue('dr_settings/config/debug', ScopeInterface::SCOPE_STORE);
-        if ($debug) {
-            $this->logger->log($level, $message, $context);
-            return true;
-        }
-        return false;
+        $this->logger = $logger;
+    }
+
+    /**
+     * Do something
+     * @return void
+     */
+    public function execute()
+    {
+        $this->logger->alert('WAKE UP!');
+        $this->logger->debug('Next step');
+        $this->logger->notice('Take a look at this');
+        $this->logger->error('Something went wrong', ['param1' => 'value1']);
     }
 }
